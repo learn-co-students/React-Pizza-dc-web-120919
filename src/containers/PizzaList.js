04455c78cb385c38e841f4e826e16bde08bc4e77
Mansoor  Cheyo <mcheyo.mc@gmail.com>
2020-02-25 15:58:39 -0500

    Done.

--- src/containers/PizzaList.js
+++ src/containers/PizzaList.js
@@ -3,6 +3,7 @@ import Pizza from '../components/Pizza'
 class PizzaList extends Component {
 
   render() {
+    
     return (
       <table className="table table-striped">
         <thead>
@@ -15,7 +16,7 @@ class PizzaList extends Component {
         </thead>
         <tbody>
           {
-            //render Pizza here
+            this.props.pizzaArray.map(pizza => <Pizza key={pizza.id} pizzaObj={pizza} grabPizzaEdit={this.props.grabPizzaEdit}/>)
           }
         </tbody>
       </table>
