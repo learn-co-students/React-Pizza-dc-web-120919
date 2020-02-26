2e1bf77a03ee4d2f619e91e654e4879765cf088e
Anna Dybas <a.m.dybas@gmail.com>
2020-02-26 13:55:20 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -32,24 +32,29 @@ class App extends Component {
   }
 
   updatePizza = (info, event) => {
-    let allPizza = [...this.state.pizzas]
+    // let allPizza = [...this.state.pizzas]
     let currentPizza = {...this.state.editPizza}
     if (event.target.type === "radio"){
       info = (event.target.value === "Not Vegetarian" && event.target.checked === true) ? false : true
     }
     currentPizza[event.target.name] = info
-    allPizza.splice(allPizza.findIndex(pizza => currentPizza.id === pizza.id), 1, currentPizza)
+    // allPizza.splice(allPizza.findIndex(pizza => currentPizza.id === pizza.id), 1, currentPizza)
     // debugger
-    this.setState({pizzas: allPizza, editPizza: currentPizza})
+    this.setState({ editPizza: currentPizza})
     console.log("checking assumptions ", currentPizza)
     console.log(this.state.editPizza)
   }
 
-  submitHandler = (event) => {
+  submitHandler = () => {
+    let allPizza = [...this.state.pizzas]
     let updatedPizza = this.state.editPizza
     let updatedBody = {topping: updatedPizza.topping,
       size: updatedPizza.size,
       vegetarian: updatedPizza.vegetarian}
+
+    allPizza.splice(allPizza.findIndex(pizza => updatedPizza.id === pizza.id), 1, updatedPizza)
+    this.setState({pizzas: allPizza })
+
     fetch(URL + "/" + updatedPizza.id, {
       method: "PATCH",
       headers: {
